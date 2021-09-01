f389a0da8f86169d52b7ad4e904354edd144d40a
Gracyele <gracyelesantossj@gmail.com>
2021-09-01 14:53:48 -0300

    add task model

---
+++ lib/model/task_model.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class TaskModel {
+  String? text;
+  String? dayOfWeek;
+  Status? status;
+  int? id;
+  Color? color;
+  DateTime? hour;
+
+  TaskModel(
+      {this.color, this.dayOfWeek, this.hour, this.id, this.status, this.text});
+
+  // TaskModel.fromDocument(DocumentSnapshot document) {
+  //   var data = document.data() as dynamic;
+  //   id = document.id as int;
+  //   hour = data['hour'] as DateTime;
+  //   dayOfWeek = data['dayOfWeek'] as String;
+  //   color = data['color'] as Color;
+  //   status = data['periodo'] as Status;
+  // }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': this.id,
+      'text': this.text,
+      'status': this.status,
+      'dayOfWeek': this.dayOfWeek,
+      'color': this.color,
+      'hour': this.hour
+    };
+  }
+}
+
+enum Status { done, blocked, todo, doing }
