4266e8711e7b00fc76ed9c3519f6cdec4e6ea0dd
Gracyele <gracyelesantossj@gmail.com>
2021-09-10 14:46:11 -0300

    fix layout and function errors

--- lib/screen/create_note/card_create_note_widget.dart
+++ lib/screen/create_note/card_create_note_widget.dart
@@ -7,14 +7,19 @@ class CardCreateNoteWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: EdgeInsets.all(40),
+      padding: EdgeInsets.all(20),
       decoration: BoxDecoration(color: Colors.white, boxShadow: [
-        BoxShadow(blurRadius: 5, color: Color(0xffE4ADF8), offset: Offset(2, 2))
+        BoxShadow(
+            blurRadius: 5,
+            color: Color(0xffE4ADF8).withOpacity(0.28),
+            offset: Offset(2, 2))
       ]),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        mainAxisSize: MainAxisSize.min,
-        children: widgets,
+      child: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: widgets,
+        ),
       ),
     );
   }
