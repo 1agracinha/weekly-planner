1d23ef293f77d9d120831e740c2b5cb1b89c0052
Gracyele <gracyelesantossj@gmail.com>
2021-09-13 16:11:19 -0300

    add remove diacritic to listTaskOfDay function

--- lib/util/function_util.dart
+++ lib/util/function_util.dart
@@ -1,31 +1,33 @@
 import 'package:flutter/material.dart';
 import 'package:weekly_planner/model/task_model.dart';
 import 'package:weekly_planner/util/list_util.dart';
+import 'package:diacritic/diacritic.dart';
 
 class FunctionUtil {
   static listTasksOfDay(List<TaskModel> tasks) {
     clearList();
     for (int i = 0; i < tasks.length; i++) {
-      var tasksIn = tasks[i].dayOfWeek as dynamic;
-      if (tasksIn.toUpperCase().contains("SEG")) {
+      var tasksIn = tasks[i].dayOfWeek?.toUpperCase() as dynamic;
+      tasksIn = removeDiacritics(tasksIn);
+      if (tasksIn.contains("SEG")) {
         ListUtil.segList.add(tasks[i]);
         print('SEG');
-      } else if (tasksIn.toUpperCase().contains("TER")) {
+      } else if (tasksIn.contains("TER")) {
         ListUtil.terList.add(tasks[i]);
         print('TER');
-      } else if (tasksIn.toUpperCase().contains("QUA")) {
+      } else if (tasksIn.contains("QUA")) {
         ListUtil.quaList.add(tasks[i]);
         print('QUAR');
-      } else if (tasksIn.toUpperCase().contains("QUI")) {
+      } else if (tasksIn.contains("QUI")) {
         ListUtil.quiList.add(tasks[i]);
         print('QUI');
-      } else if (tasksIn.toUpperCase().contains("SEX")) {
+      } else if (tasksIn.contains("SEX")) {
         ListUtil.sexList.add(tasks[i]);
         print('SEX');
-      } else if (tasksIn.toUpperCase().contains("SAB")) {
+      } else if (tasksIn.contains("SAB")) {
         ListUtil.sabList.add(tasks[i]);
         print('SAB');
-      } else if (tasksIn.toUpperCase().contains("DOM")) {
+      } else if (tasksIn.contains("DOM")) {
         ListUtil.domList.add(tasks[i]);
         print('DOM');
       }
