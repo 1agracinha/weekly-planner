0e5b74128cd41f27d30ed93b4c2d113a388c7daf
Gracyele <gracyelesantossj@gmail.com>
2021-09-17 15:23:49 -0300

    change addNote access to data Class

--- lib/controller/create_note_controller.dart
+++ lib/controller/create_note_controller.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:weekly_planner/data/create_note_data.dart';
 import 'package:weekly_planner/model/create_note/color_item_model.dart';
 import 'package:weekly_planner/model/create_note/date_item_model.dart';
 import 'package:weekly_planner/model/create_note/hour_item_model.dart';
@@ -10,20 +11,20 @@ import 'package:weekly_planner/shared/custom_dialogs.dart';
 
 class CreateNoteController extends GetxController {
   Rx<String> id = "".obs;
+  Rx<int> indice = 0.obs;
+  Rx<Status> status = Status.todo.obs;
+  Rx<String> textNote = "undefined".obs;
+  var listTaskModel = <TaskModel>[].obs;
+  CreateNoteData createNoteData = CreateNoteData();
   Rx<ColorItemModel> colorItemModel =
       ColorItemModel(color: Mock.colors[0].color, value: 0).obs;
   Rx<DateItemModel> dateItemModel =
       DateItemModel(date: "segunda", value: 0).obs;
   Rx<HourItemModel> hourItemModel = HourItemModel(hour: "00:00", value: 0).obs;
-  var listTaskModel = <TaskModel>[].obs;
-  Rx<String> textNote = "undefined".obs;
-  Rx<int> indice = 0.obs;
-
-  Rx<Status> status = Status.todo.obs;
 
   Future<bool> addNote(BuildContext context) async {
     bool isSuccess = false;
-    TaskModel obj = TaskModel(
+    TaskModel note = TaskModel(
       id: id.value,
       color: colorItemModel.value.color,
       dayOfWeek: dateItemModel.value.date,
@@ -32,10 +33,7 @@ class CreateNoteController extends GetxController {
       text: textNote.value,
     );
 
-    await FirebaseFirestore.instance
-        .collection('notas')
-        .add(toMap(obj))
-        .then((data) {
+    createNoteData.addNote(toMap(note)).then((data) {
       id.value = data.id;
       if (id.value.isNotEmpty) {
         CustomDialogs.successOperation(
@@ -56,7 +54,7 @@ class CreateNoteController extends GetxController {
     return isSuccess;
   }
 
-  Map<String, dynamic> toMap(TaskModel obj) {
+  static Map<String, dynamic> toMap(TaskModel obj) {
     return {
       'color': "${obj.color}",
       'dayOfWeek': "${obj.dayOfWeek}",
