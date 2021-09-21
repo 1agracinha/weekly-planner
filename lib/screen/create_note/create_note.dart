a832606db9d29a2d4d3763f200c8f4eccf66617a
Gracyele <gracyelesantossj@gmail.com>
2021-09-21 13:10:19 -0300

    set getbuilder controller  inside the page

--- lib/screen/create_note/create_note.dart
+++ lib/screen/create_note/create_note.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:weekly_planner/controller/create_note_controller.dart';
 import 'package:weekly_planner/screen/create_note/card_create_note_widget.dart';
 import 'package:weekly_planner/screen/create_note/select_color_dropdown_widget.dart';
@@ -8,31 +9,33 @@ import 'package:weekly_planner/screen/create_note/text_input_widget.dart';
 import 'package:weekly_planner/screen/create_note/text_title_widget.dart';
 import 'package:weekly_planner/shared/custom_button_widget.dart';
 
-class CreateNote extends StatefulWidget {
-  final CreateNoteController createNoteController;
-  CreateNote(this.createNoteController);
-  @override
-  _CreateNoteState createState() => _CreateNoteState();
-}
-
-class _CreateNoteState extends State<CreateNote> {
-  @override
+class CreateNote extends StatelessWidget {
   Widget build(BuildContext context) {
-    return Form(
-        child: CardCreateNoteWidget(
-      widgets: [
-        TextTitleWidget(),
-        TextInputWidget(widget.createNoteController),
-        SelectColorDropdownWidget(widget.createNoteController),
-        SelectHourDropdownWidget(widget.createNoteController),
-        SelectDateDropdownWidget(widget.createNoteController),
-        CustomButtonWidget(
-          onPressed: () {
-            widget.createNoteController.addNote(context);
-          },
-          text: "add note",
-        )
-      ],
-    ));
+    return GetBuilder<CreateNoteController>(
+      builder: (createNoteController) {
+        return Container(
+          width: MediaQuery.of(context).size.width * 0.3,
+          padding: EdgeInsets.symmetric(
+              horizontal: MediaQuery.of(context).size.width * 0.025),
+          child: Form(
+            child: CardCreateNoteWidget(
+              widgets: [
+                TextTitleWidget(),
+                TextInputWidget(createNoteController),
+                SelectColorDropdownWidget(createNoteController),
+                SelectHourDropdownWidget(createNoteController),
+                SelectDateDropdownWidget(createNoteController),
+                CustomButtonWidget(
+                  onPressed: () {
+                    createNoteController.addNote(context);
+                  },
+                  text: "add note",
+                )
+              ],
+            ),
+          ),
+        );
+      },
+    );
   }
 }
