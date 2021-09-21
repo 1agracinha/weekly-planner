9c707be71a6ced8eceb593c884049df5d0835263
Gracyele <gracyelesantossj@gmail.com>
2021-09-21 13:12:00 -0300

    add home controller to MyHomePage

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:weekly_planner/controller/create_note_controller.dart';
 import 'package:weekly_planner/screen/banner/weekly_banner.dart';
 import 'package:weekly_planner/screen/create_note/create_note.dart';
+import 'package:weekly_planner/shared/custom_appbar_widget.dart';
 import 'package:weekly_planner/util/height_error.dart';
 import 'package:get/get.dart';
+
+import 'controller/home_controller.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -17,10 +18,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      title: 'Weekly Planner',
       debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
@@ -35,53 +33,31 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: PreferredSize(
-        child: Container(
-          decoration: BoxDecoration(boxShadow: [
-            BoxShadow(
-              color: Color(0xffDDB8E7),
-              offset: Offset(0, 4),
-              blurRadius: 6,
-            )
-          ]),
-          child: AppBar(
-            backgroundColor: Color(0xffDDB8E7),
-            elevation: 0.0,
-            title: Center(
-                child: Text(
-              "Weekly Planner",
-              style: GoogleFonts.getFont('Rochester',
-                  color: Colors.white, fontSize: 24),
-            )),
+    return GetBuilder<HomeController>(
+      init: HomeController(),
+      builder: (_) {
+        return Scaffold(
+          appBar: PreferredSize(
+            child: CustomAppBarWidget(),
+            preferredSize: Size.fromHeight(kToolbarHeight),
           ),
-        ),
-        preferredSize: Size.fromHeight(kToolbarHeight),
-      ),
-      body: MediaQuery.of(context).size.height < 430 ||
-              MediaQuery.of(context).size.width < 930
-          ? HeightErrorScreen()
-          : GetBuilder<CreateNoteController>(
-              init: CreateNoteController(),
-              builder: (createNoteController) {
-                return Padding(
+          body: MediaQuery.of(context).size.height < 430 ||
+                  MediaQuery.of(context).size.width < 930
+              ? HeightErrorScreen()
+              : Padding(
                   padding: const EdgeInsets.all(20),
                   child: Row(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
-                      Container(
-                          width: MediaQuery.of(context).size.width * 0.3,
-                          padding: EdgeInsets.symmetric(
-                              horizontal:
-                                  MediaQuery.of(context).size.width * 0.025),
-                          child: CreateNote(createNoteController)),
+                      CreateNote(),
                       Container(
                           width: MediaQuery.of(context).size.width * 0.65,
-                          child: WeeklyBanner(createNoteController))
+                          child: WeeklyBanner())
                     ],
                   ),
-                );
-              }),
+                ),
+        );
+      },
     );
   }
 }
