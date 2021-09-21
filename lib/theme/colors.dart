0e2bde41c25835a29fa292f05a024815bc5dbf99
Gracyele <gracyelesantossj@gmail.com>
2021-09-21 13:26:15 -0300

    add theme color

---
+++ lib/theme/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+abstract class ColorUtil {
+  static Color mainPurple = Color(0xffDDB8E7);
+  static Color mainGray = Color(0xffC4C4C4);
+  static Color mainStrongPurple = Color(0xff8600A8);
+}
