7b0ca4625e966dfc2a057e87c72ab8f09d2521aa
Gracyele <gracyelesantossj@gmail.com>
2021-09-21 13:05:39 -0300

    create convert function to status and color

--- lib/model/task_model.dart
+++ lib/model/task_model.dart
@@ -8,17 +8,45 @@ class TaskModel {
   Color? color;
   String? hour;
 
-  TaskModel(
-      {this.color, this.dayOfWeek, this.hour, this.id, this.status, this.text});
+  TaskModel({
+    this.color,
+    this.dayOfWeek,
+    this.hour,
+    this.id,
+    this.status,
+    this.text,
+  });
 
-  // TaskModel.fromDocument(DocumentSnapshot document) {
-  //   var data = document.data() as dynamic;
-  //   id = document.id as int;
-  //   hour = data['hour'] as DateTime;
-  //   dayOfWeek = data['dayOfWeek'] as String;
-  //   color = data['color'] as Color;
-  //   status = data['periodo'] as Status;
-  // }
+  TaskModel fromDocument(data) {
+    return TaskModel(
+      color: convertStringToColor(data["color"]),
+      dayOfWeek: data["dayOfWeek"],
+      hour: data["hour"],
+      status: convertStringToStatus(data["status"]),
+      text: data["text"],
+    );
+  }
+
+  Color convertStringToColor(String stringColor) {
+    int convertString = int.parse(
+      stringColor.replaceAll("Color(", "").replaceAll(")", ""),
+    );
+    return Color(convertString);
+  }
+
+  Status? convertStringToStatus(String stringStatus) {
+    Status? convertStatus;
+
+    if (stringStatus.contains("todo"))
+      convertStatus = Status.todo;
+    else if (stringStatus.contains("doing"))
+      convertStatus = Status.doing;
+    else if (stringStatus.contains("done"))
+      convertStatus = Status.done;
+    else if (stringStatus.contains("blocked")) convertStatus = Status.blocked;
+
+    return convertStatus;
+  }
 
   Map<String, dynamic> toMap() {
     return {
