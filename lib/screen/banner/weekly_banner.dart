fd84fd2e29de3814e2b5fba3a9f76bbcd2f4f572
Gracyele <gracyelesantossj@gmail.com>
2021-09-21 13:14:34 -0300

    add inner controller and update widgets

--- lib/screen/banner/weekly_banner.dart
+++ lib/screen/banner/weekly_banner.dart
@@ -1,123 +1,101 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:sticky_headers/sticky_headers/widget.dart';
-import 'package:weekly_planner/controller/create_note_controller.dart';
+import 'package:weekly_planner/controller/list_note_controller.dart';
 import 'package:weekly_planner/model/task_model.dart';
 import 'package:weekly_planner/screen/banner/task_widget.dart';
-import 'package:weekly_planner/util/function_util.dart';
 
-class WeeklyBanner extends StatefulWidget {
-  CreateNoteController createNoteController;
-  WeeklyBanner(this.createNoteController);
-  @override
-  _WeeklyBannerState createState() => _WeeklyBannerState();
-}
-
-class _WeeklyBannerState extends State<WeeklyBanner> {
-  @override
-  void initState() {
-    FunctionUtil.listTasksOfDay(widget.createNoteController.listTaskModel);
-    super.initState();
-  }
-
+class WeeklyBanner extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Obx(() {
-      print(FunctionUtil.listTasksOfDay(
-          widget.createNoteController.listTaskModel));
-      return Container(
-        width: MediaQuery.of(context).size.width,
-        color: Colors.white,
+    return GetBuilder<ListNoteController>(
+        builder: (listNoteController) => Container(
+              width: MediaQuery.of(context).size.width,
+              color: Colors.white,
+              child: SingleChildScrollView(
+                child: Column(
+                  children: [
+                    StickyHeader(
+                      header: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: listDaysOfWeek(),
+                      ),
+                      content: Obx(
+                        () => Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              dayOfWeekColumn("SEG",
+                                  listNoteController.mondayList, context),
+                              dayOfWeekColumn("TER",
+                                  listNoteController.tuesdayList, context),
+                              dayOfWeekColumn("QUA",
+                                  listNoteController.wednesdayList, context),
+                              dayOfWeekColumn("QUI",
+                                  listNoteController.thursdayList, context),
+                              dayOfWeekColumn("SEX",
+                                  listNoteController.fridayList, context),
+                              dayOfWeekColumn("SAB",
+                                  listNoteController.saturdayList, context),
+                              dayOfWeekColumn("DOM",
+                                  listNoteController.sundayList, context),
+                            ]),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ));
+  }
+
+  Widget dayOfWeekColumn(
+      String title, List<TaskModel> tasksOfDay, BuildContext context) {
+    return Flexible(
+      child: Container(
+        height: MediaQuery.of(context).size.height,
+        decoration: BoxDecoration(
+            border: Border(
+                right: BorderSide(width: 0.5, color: Colors.grey),
+                top: BorderSide(width: 0.5, color: Colors.grey))),
         child: SingleChildScrollView(
           child: Column(
-            children: [
-              StickyHeader(
-                header: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: getDaysOfWeek()),
-                content: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: columnDayOfWeek(context)),
-              )
-            ],
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: tasksOfDay.length > 0
+                ? tasksOfDay.map((task) => TaskWidget(taskModel: task)).toList()
+                : freeDay(),
           ),
         ),
-      );
-    });
-  }
-
-  List<Widget> tasksDayOfWeek(List<TaskModel> tasks, context) {
-    List<Widget> taskOfDay = <Widget>[];
-    for (int i = 0; i < tasks.length; i++) {
-      taskOfDay.add(TaskWidget(taskModel: tasks[i]));
-    }
-
-    return taskOfDay;
-  }
-
-  List<Widget> columnDayOfWeek(BuildContext context) {
-    List<Widget> colunas = [];
-
-    for (int i = 0; i <= 6; i++) {
-      colunas.add(
-        Flexible(
-          child: Container(
-            height: MediaQuery.of(context).size.height,
-            decoration: BoxDecoration(
-                border: Border(
-                    right: BorderSide(width: 0.5, color: Colors.grey),
-                    top: BorderSide(width: 0.5, color: Colors.grey))),
-            // child: Column(
-            //   crossAxisAlignment: CrossAxisAlignment.center,
-            //   children: FunctionUtil.listTasksDayOfWeek(context)[i].length > 0
-            //       ? tasksDayOfWeek(
-            //           FunctionUtil.listTasksDayOfWeek(context)[i], context)
-            //       : freeDay(),
-            // ),
-            child: SingleChildScrollView(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: FunctionUtil.listTasksDayOfWeek(context)[i].length > 0
-                    ? tasksDayOfWeek(
-                        FunctionUtil.listTasksDayOfWeek(context)[i], context)
-                    : freeDay(),
-              ),
-            ),
-          ),
-        ),
-      );
-    }
-
-    return colunas;
+      ),
+    );
   }
 
   List<Widget> freeDay() {
     return [
       TaskWidget(
+          enable: false,
           taskModel: TaskModel(
-        color: Colors.transparent,
-        status: Status.todo,
-        dayOfWeek: "",
-        hour: "00:00",
-        text: "",
-        id: "0",
-      ))
+            color: Colors.transparent,
+            status: Status.todo,
+            dayOfWeek: "",
+            hour: "00:00",
+            text: "",
+            id: "0",
+          ))
     ];
   }
 
-  List<Widget> getDaysOfWeek() {
+  List<Widget> listDaysOfWeek() {
     List<String> daysOfWeek = ["SEG", "TER", "QUA", "QUI", "SEX", "SAB", "DOM"];
     List<Widget> widgetDaysOfWeek = <Widget>[];
 
     for (int i = 0; i < daysOfWeek.length; i++) {
-      widgetDaysOfWeek.add(dayOfWeekTitle(daysOfWeek[i]));
+      widgetDaysOfWeek.add(dayOfWeekTitleWidget(daysOfWeek[i]));
     }
 
     return widgetDaysOfWeek;
   }
 
-  Widget dayOfWeekTitle(String semana) {
+  Widget dayOfWeekTitleWidget(String semana) {
     return Flexible(
       child: Container(
         alignment: Alignment.center,
