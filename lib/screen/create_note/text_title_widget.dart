1143f08575ba8b7509ae09a7e7fc8e21f9dd9951
Gracyele <gracyelesantossj@gmail.com>
2021-09-22 10:29:24 -0300

    add title param to text title widget

--- lib/screen/create_note/text_title_widget.dart
+++ lib/screen/create_note/text_title_widget.dart
@@ -2,12 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:weekly_planner/theme/colors.dart';
 
 class TextTitleWidget extends StatelessWidget {
+  final String title;
+  TextTitleWidget(this.title);
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.only(bottom: 12),
       child: Text(
-        "Adicionar nota à semana?",
+        title,
         style: TextStyle(color: ColorUtil.mainGray, fontSize: 26),
       ),
     );
